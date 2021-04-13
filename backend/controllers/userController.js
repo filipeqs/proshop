b4e65c42b510e0608446c950480cae124c584d25
Filipe Silva <filipeqs@outlook.com>
2021-04-13 19:22:38 -0400

    auth user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,24 @@
 import asyncHandler from 'express-async-handler';
 
-import User from '../models/userModel';
+import User from '../models/userModel.js';
 
-const authUser = asyncHandler(async (req, res) => {});
+const authUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        return res.send({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: null,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
+});
+
+export { authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,3 +1,12 @@
+import express from 'express';
+
+import { authUser } from '../controllers/userController.js';
+
+const router = express.Router();
+
 // @desc    Auth user and get Token
 // @route   Post /api/users/login
 // @access  Public
+router.route('/login').post(authUser);
+
+export default router;
