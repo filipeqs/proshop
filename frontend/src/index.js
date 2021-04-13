330bbcee1d4e448d5acf3fbdcdc3739916a5983e
Filipe Silva <filipeqs@outlook.com>
2021-04-13 09:37:13 -0400

    implementing redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './bootstrap.min.css';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App';
+import './bootstrap.min.css';
+import './index.css';
+
+import store from './redux/store';
+
 ReactDOM.render(
-    <React.StrictMode>
+    <Provider store={store}>
         <App />
-    </React.StrictMode>,
+    </Provider>,
     document.getElementById('root'),
 );
 
