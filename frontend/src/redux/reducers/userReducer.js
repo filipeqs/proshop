32f2a5b4e65c286fef780aa591a29a1dd387146e
Filipe Silva <filipeqs@outlook.com>
2021-04-26 11:16:02 -0400

    list users screen

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -13,6 +13,9 @@ import {
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_DETAILS_RESET,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -112,6 +115,35 @@ export const userUpdateProfileReducer = (state = {}, action) => {
                 userInfo: action.payload,
             };
         case USER_UPDATE_PROFILE_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export const userListReducer = (
+    state = {
+        users: [],
+    },
+    action,
+) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case USER_LIST_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                users: action.payload,
+            };
+        case USER_LIST_FAIL:
             return {
                 ...state,
                 loading: false,
