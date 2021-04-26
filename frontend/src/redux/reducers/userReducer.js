e7082426705cbae324cf78559b5704ae04d7de91
Filipe Silva <filipeqs@outlook.com>
2021-04-26 18:13:01 -0400

    update user screen and action

--- frontend/src/redux/reducers/userReducer.js
+++ frontend/src/redux/reducers/userReducer.js
@@ -19,6 +19,10 @@ import {
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
     USER_DELETE_FAIL,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS,
+    USER_UPDATE_FAIL,
+    USER_UPDATE_RESET,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -175,6 +179,35 @@ export const userDeleteReducer = (state = {}, action) => {
                 ...state,
                 loading: false,
                 error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export const userUpdateReducer = (state = {}, action) => {
+    switch (action.type) {
+        case USER_UPDATE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case USER_UPDATE_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                success: true,
+            };
+        case USER_UPDATE_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        case USER_UPDATE_RESET:
+            return {
+                ...state,
+                success: false,
             };
         default:
             return state;
---
+++ frontend/src/screens/UserEditScreen.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import FormContainer from '../components/FormContainer';
+
+import { USER_UPDATE_RESET } from '../redux/constants/userConstants';
+import { getUserDetails, updateUser } from '../redux/actions/userActions';
+
+const UserEditScreen = ({ history, match }) => {
+    const dispatch = useDispatch();
+
+    const userId = match.params.id;
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [isAdmin, setIsAdmin] = useState(false);
+
+    const userDetails = useSelector((state) => state.userDetails);
+    const { loading, error, user } = userDetails;
+
+    const userUpdate = useSelector((state) => state.userUpdate);
+    const { loading: loadingUpdate, error: errorUpdate, success: successUpdate } = userUpdate;
+
+    useEffect(() => {
+        if (successUpdate) {
+            dispatch({ type: USER_UPDATE_RESET });
+            history.push('/admin/userlist');
+        } else {
+            if (!user.name || user._id !== userId) {
+                dispatch(getUserDetails(userId));
+            } else {
+                setName(user.name);
+                setEmail(user.email);
+                setIsAdmin(user.isAdmin);
+            }
+        }
+    }, [dispatch, user, userId, successUpdate, history]);
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        dispatch(updateUser({ _id: userId, name, email, isAdmin }));
+    };
+
+    return (
+        <Fragment>
+            <Link to="/admin/userlist" className="btn btn-light my3">
+                Go Back
+            </Link>
+            <FormContainer>
+                <h1>Edit User</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+                {loading ? (
+                    <Loader />
+                ) : error ? (
+                    <Message variant="dante">{error}</Message>
+                ) : (
+                    <Form onSubmit={submitHandler}>
+                        <Form.Group controlId="name">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control
+                                type="name"
+                                placeholder="Enter Name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            ></Form.Control>
+                        </Form.Group>
+                        <Form.Group controlId="isAdmin">
+                            <Form.Check
+                                type="checkbox"
+                                label="Is Admin"
+                                checked={isAdmin}
+                                onChange={(e) => setIsAdmin(e.target.checked)}
+                            ></Form.Check>
+                        </Form.Group>
+
+                        <Button type="submit" variant="primary">
+                            Update
+                        </Button>
+                    </Form>
+                )}
+            </FormContainer>
+        </Fragment>
+    );
+};
+
+export default UserEditScreen;
