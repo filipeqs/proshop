c1836d0aabd89605fbb80b69478c9cc7a1c7be14
Filipe Silva <filipeqs@outlook.com>
2021-05-02 09:42:52 -0400

    upload image fie

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,6 @@
+import path from 'path';
 import express from 'express';
 import multer from 'multer';
-import path from 'path';
 
 const router = express.Router();
 
@@ -9,16 +9,16 @@ const storage = multer.diskStorage({
         cb(null, 'uploads/');
     },
     filename(req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now}${path.extname(file.originalname)}`);
+        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     },
 });
 
 function checkFileType(file, cb) {
-    const fileTypes = /jpg|jpeg|png/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimeType = fileTypes.test(file.mimeType);
+    const filetypes = /jpg|jpeg|png/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
 
-    if (extname && mimeType) {
+    if (extname && mimetype) {
         return cb(null, true);
     } else {
         cb('Images only!');
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -9,6 +9,7 @@ import FormContainer from '../components/FormContainer';
 
 import { getProductDetails, updateProduct } from '../redux/actions/productActions';
 import { PRODUCT_DETAILS_RESET, PRODUCT_UPDATE_RESET } from '../redux/constants/productConstants';
+import axios from 'axios';
 
 const ProductEditScreen = ({ history, match }) => {
     const dispatch = useDispatch();
@@ -22,6 +23,7 @@ const ProductEditScreen = ({ history, match }) => {
     const [category, setCategory] = useState('');
     const [countInStock, setCountInStock] = useState(0);
     const [description, setDescription] = useState('');
+    const [uploading, setUploading] = useState(false);
 
     const productDetails = useSelector((state) => state.productDetails);
     const { loading, error, product } = productDetails;
@@ -50,6 +52,28 @@ const ProductEditScreen = ({ history, match }) => {
             dispatch({ type: PRODUCT_DETAILS_RESET });
         };
     }, [dispatch, product, productId, history, successUpdate]);
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            };
+
+            const { data } = await axios.post('/api/upload', formData, config);
+            setImage(data);
+            setUploading(false);
+        } catch (error) {
+            console.error(error);
+            setUploading(false);
+        }
+    };
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -109,6 +133,13 @@ const ProductEditScreen = ({ history, match }) => {
                                 value={image}
                                 onChange={(e) => setImage(e.target.value)}
                             ></Form.Control>
+                            <Form.File
+                                id="image-file"
+                                label="Choose File"
+                                custom
+                                onChange={uploadFileHandler}
+                            ></Form.File>
+                            {uploading && <Loader />}
                         </Form.Group>
 
                         <Form.Group controlId="brand">
