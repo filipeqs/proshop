6c7a7eaea1489ed4fcf4399a484b5dbd23bbbeaf
Filipe Silva <filipeqs@outlook.com>
2021-05-11 09:07:14 -0400

    go back button when search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -27,7 +28,13 @@ const HomeScreen = ({ match }) => {
     return (
         <Fragment>
             <Meta />
-            {!keyword && <ProductCarousel />}
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to="/" className="btn btn-light">
+                    Go Back
+                </Link>
+            )}
             <h1>Latest Products</h1>
             {loading ? (
                 <Loader />
